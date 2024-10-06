1554d34d8544e15232f396f133fbd21625816e78
Aylin Onalan <72365521+aonalan@users.noreply.github.com>
2024-10-06 16:52:19 -0700

    Working HW1 code

--- HW1_Folder/HW1_App/App.js
+++ HW1_Folder/HW1_App/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*  I also had to install the following dependancies so that I could
+    switch between pages in the application on android:
+      npm install @react-navigation/native
+      npm install @react-navigation/stack
+      npm install react-native-screens react-native-safe-area-context
+      npm install react-native-gesture-handler react-native-reanimated
+ */
 
-export default function App() {
+// nessesery for React and navigation between screens to work
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+// imports the other two JS files for the 2 screens
+import Screen1 from './Screen1';
+import Screen2 from './Screen2';
+
+const Stack = createNativeStackNavigator();
+
+// initiates the 2 screens and starts the application on "Screen1"
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen1">
+        <Stack.Screen name="Screen1" component={Screen1} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ HW1_Folder/HW1_App/Screen2.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+// creates screen with a "button" that when pressed takes you to Screen1
+function Screen2({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => navigation.navigate('Screen1')}
+      >
+        <Text style={styles.buttonText}>Click Me!</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+// styles to set the button color to yellow and the text to black
+// as well as to center the button
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'yellow',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'black',  
+    fontSize: 18,
+  },
+});
+
+export default Screen2;
